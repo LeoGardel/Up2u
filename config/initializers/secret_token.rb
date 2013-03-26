165957a1ca8b26711ca27d4fcc08813bbac49ddc
LeoGardel <leovalverde@poli.ufrj.br>
2013-03-26 02:44:40 -0300

    Versão inicial dos arquivos do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Up2u::Application.config.secret_token = 'b5be9abb44b3f33906a6d10243f9b6c5f2b6327eac7f2d040a62f1703744affea86b0696bec152952dfdb1a3d941d07e8c3ca55bb08753ed837d9437c31044c3'
