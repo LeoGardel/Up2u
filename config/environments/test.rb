c8c1f0d55d4dca8b3797115a1bd49bb190272bda
Leonardo Valverde <leovalverde@poli.ufrj.br>
2013-04-09 00:04:10 -0300

    adicionando configuraoes de envio de email em production

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,17 @@ Up2u::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'carreiraup2u@gmail.com',
+    :password             => 'carreira2012',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+end
+
 end
