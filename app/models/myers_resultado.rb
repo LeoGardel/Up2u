8c0295dfd76442ee14d5df32d75767b0dd606de5
Leonardo Valverde <leovalverde@poli.ufrj.br>
2013-08-06 18:20:21 -0300

    calculo dos resultados de perfil sendo realizado, e salvando na tabela myers_resultados

---
+++ app/models/myers_resultado.rb
@@ -0,0 +1,15 @@
+class MyersResultado < ActiveRecord::Base
+  belongs_to :usuario
+  belongs_to :myers_tipo
+
+  attr_accessible :fator, :myers_tipo_id, :usuario_id
+
+  def self.salva_hash_resultados(usuario_id, hash)
+    hash.each_key { |key| 
+      @resultado = MyersResultado.new(myers_tipo_id: key,
+                         usuario_id: usuario_id,
+                         fator: ( hash[key][0] + ( 2 * hash[key][1] ) ) * 100 / ( 4 * hash[key][1] ).to_f )
+      @resultado.save
+    }
+  end
+end
